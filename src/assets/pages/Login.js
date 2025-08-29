875dd7d87d6a608f8385d49092bc334cf45ffc58
Aluno <thayna.matos.carlos@gmail.com>
2025-08-29 07:33:04 -0300

    Alterando a programação

--- src/assets/pages/Login.js
+++ src/assets/pages/Login.js
@@ -1,19 +1,55 @@
+import { StatusBar } from 'expo-status-bar';
 import { Text, ImageBackground, TouchableOpacity, View, Pressable } from 'react-native';
 
-// IMPORTAÇÃO REACT NATIVE
-import { useNavigation } from '@react-navigation/native';
+// IMPORTAÇÃO COMPONENTES
+import InputComp from '../components/InputComp';
+import TextComp from '../components/TextComp';
 
-//IMPORTAÇÃO StyleSheet
+// IMPORTANDO O ICONE
+import Foundation from '@expo/vector-icons/Foundation';
+
 import { styles } from '../styles/styles';
 
-export default function Login() {
+// IMPORTAÇÃO NATIVE
+import { useNavigation } from '@react-navigation/native';
+import { setItem } from '../components/AsyncStorage';
 
-  const Navigation = useNavigation(); 7
+// IMPORTAÇÃO DE ICONE
+import MaterialIcons from '@expo/vector-icons/MaterialIcons';
 
+
+export default function SignIn() {
+
+  const navigation = useNavigation();
+
+  // FAZENDO FUNÇÃO DO BOTÃO ENVIAR PARA PAGINA HOME
+  const handleLogin = async () => {
+
+    await setItem("login", "1")
+    navigation.navigate("Home")
+  }
   return (
-    <View>z0
+    <ImageBackground style={styles.containerSI} source={require("../images/LoginScreen.jpg")}>
+      <MaterialIcons name="movie" size={100} color="white" style={{ left: -120, bottom: 180 }} />
 
-    </View>
 
+
+      <Text style={styles.tituloSI}>ACESSE SUA CONTA!</Text>
+      <Text style={styles.subTituloSI}> Bem vindo de volta usuário!</Text>
+
+      <View style={styles.campo}>
+        <TextComp txt="Email:" />
+        <InputComp textPlaceHolder={"Digite seu email"} password={false} />
+        <TextComp txt="Senha:" />
+        <InputComp textPlaceHolder={"Digite sua senha"} password={true} />
+      </View>
+
+      <TouchableOpacity onPress={handleLogin} style={styles.btnSI} >
+        <Text style={styles.cadastroSI}> ENTRAR </Text>
+      </TouchableOpacity>
+
+    </ImageBackground>
   );
 }
+
+//
